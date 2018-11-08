2bdf0b39b83d454d894258d857d1e4676207fbbe
Evgeni DImitrov <evgeni.dimitrov@nevexis.com>
2018-11-08 15:04:13 +0200

    fix integration test to use the postgres container

--- src/test/java/com/github/evgdim/tdd/PostgresIntegrationTests.java
+++ src/test/java/com/github/evgdim/tdd/PostgresIntegrationTests.java
@@ -4,48 +4,43 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.Duration;
 
-import javax.sql.DataSource;
-
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.testcontainers.containers.PostgreSQLContainer;
 
 import com.github.evgdim.tdd.repository.PersonRepository;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes= {TddReferenceApplication.class})
+@SpringBootTest(classes = { TddReferenceApplication.class })
+@ContextConfiguration(initializers = {PostgresIntegrationTests.Initializer.class })
 public class PostgresIntegrationTests {
 	@ClassRule
-	public static PostgreSQLContainer postgres = new PostgreSQLContainer<>("postgres:10.4")
-			.withDatabaseName("sampledb")
-            .withUsername("sampleuser")
-            .withPassword("samplepwd")
-            .withStartupTimeout(Duration.ofSeconds(600));
-	
-	@Configuration
-	public static class TestConfig {
-		@Bean
-		public DataSource containerDataSource() { 
-			return DataSourceBuilder.create()
-			.url(postgres.getJdbcUrl())
-			.username(postgres.getUsername())
-			.password(postgres.getPassword())
-			.driverClassName("org.postgresql.Driver")
-			.build();
+	public static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer<>("postgres:10.4").withDatabaseName("sampledb")
+			.withUsername("sampleuser").withPassword("samplepwd").withStartupTimeout(Duration.ofSeconds(600));
+
+	static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+		public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+			TestPropertyValues
+					.of("spring.datasource.url=" + postgreSQLContainer.getJdbcUrl(),
+							"spring.datasource.username=" + postgreSQLContainer.getUsername(),
+							"spring.datasource.password=" + postgreSQLContainer.getPassword())
+					.applyTo(configurableApplicationContext.getEnvironment());
 		}
-		
 	}
+
 	@Autowired
 	private PersonRepository personRepo;
+
 	@Test
 	public void personRepoCount_shouldReturn0() {
-		assertThat( personRepo.count()).isEqualTo(0);
+		assertThat(personRepo.count()).isEqualTo(0);
 	}
 }
