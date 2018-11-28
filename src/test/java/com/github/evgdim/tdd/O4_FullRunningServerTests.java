b36a2a4c587a020d31c4829890a1b88cf7be28a3
Evgeni DImitrov <evgeni.dimitrov@nevexis.com>
2018-11-28 14:40:57 +0200

    switched to mockserver

--- src/test/java/com/github/evgdim/tdd/O4_FullRunningServerTests.java
+++ src/test/java/com/github/evgdim/tdd/O4_FullRunningServerTests.java
@@ -1,27 +1,35 @@
 package com.github.evgdim.tdd;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
-import static com.github.tomakehurst.wiremock.client.WireMock.get;
-import static com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor;
-import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
-import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
-import static com.github.tomakehurst.wiremock.client.WireMock.verify;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+import org.apache.commons.io.IOUtils;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockserver.junit.MockServerRule;
+import org.mockserver.matchers.MatchType;
+import org.mockserver.model.HttpRequest;
+import org.mockserver.model.HttpResponse;
+import org.mockserver.model.JsonBody;
+import org.mockserver.model.StringBody;
+import org.mockserver.verify.VerificationTimes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.github.evgdim.tdd.entity.Person;
-import com.github.tomakehurst.wiremock.junit.WireMockRule;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, properties = {
@@ -29,7 +37,7 @@ import com.github.tomakehurst.wiremock.junit.WireMockRule;
 })
 public class O4_FullRunningServerTests {
 	@Rule
-	public WireMockRule wireMockRule = new WireMockRule(8888, 8889);
+	public MockServerRule mockServer = new MockServerRule(this, 8888);
 	
 	@Autowired
 	private TestRestTemplate restTemplate;
@@ -41,29 +49,42 @@ public class O4_FullRunningServerTests {
 	}
 	
 	@Test
-	public void externalSystemPeople_shoudlReturn200AndAPerson() {
-		stubFor(get(urlEqualTo("/people"))
-	            .willReturn(aResponse()
-	                .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
-	                .withBodyFile("person.json")));
-	            
-		ResponseEntity<Person> entity = this.restTemplate.getForEntity("/external", Person.class);
-		assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
-		assertThat(entity.getBody().getAge()).isEqualTo(29);
-		assertThat(entity.getBody().getName()).isEqualTo("Evgeni");
+	public void externalSystemPeople_shoudlReturn200AndAPerson() throws IOException {
+		try(InputStream personFileStream = new ClassPathResource("__files/person.json").getInputStream()) {
+			String personData = IOUtils.toString(personFileStream, StandardCharsets.UTF_8);
+			mockServer.getClient().when(HttpRequest.request()
+													.withMethod("GET")
+													.withPath("/people"))
+			                      .respond(HttpResponse.response()
+			                    		  				.withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+			                    		  				.withBody(personData));
+		            
+			ResponseEntity<Person> entity = this.restTemplate.getForEntity("/external", Person.class);
+			assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+			assertThat(entity.getBody().getAge()).isEqualTo(29);
+			assertThat(entity.getBody().getName()).isEqualTo("Evgeni");
+		}
 	}
 	
 	@Test
-	public void externalSystemPeople_shoudlBeCalled() {
-		stubFor(get(urlEqualTo("/people"))
-	            .willReturn(aResponse()
-	                .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
-	                .withBodyFile("person.json")));
+	public void externalSystemPeople_shoudlBeCalled() throws IOException {
+		try(InputStream personFileStream = new ClassPathResource("__files/person.json").getInputStream()) {
+			String personData = IOUtils.toString(personFileStream, StandardCharsets.UTF_8);
+			
+			mockServer.getClient().when(HttpRequest.request()
+													.withMethod("GET")
+													.withPath("/people"))
+									  .respond(HttpResponse.response()
+											  				.withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+											  				.withBody(personData));
+			
+			this.restTemplate.getForEntity("/external", Person.class);
+			
+			mockServer.getClient().verify(
+					HttpRequest.request().withMethod("GET").withPath("/people"), 
+					VerificationTimes.exactly(1)
+			);
+		}
 	            
-		ResponseEntity<Person> entity = this.restTemplate.getForEntity("/external", Person.class);
-		
-		verify(getRequestedFor(urlEqualTo("/people")).withoutHeader(MediaType.APPLICATION_JSON_VALUE));
-		//verify(1, getRequestedFor(urlEqualTo("/people")));
-		//verify(getRequestedFor(urlEqualTo("/people")).withRequestBody(WireMock.matchingJsonPath("$.whatever")));
 	}
 }
