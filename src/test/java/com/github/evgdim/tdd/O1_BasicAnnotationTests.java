15da5a3cf4b0ef5651f09533e14ccaaa77a71e00
Evgeni DImitrov <evgeni.dimitrov@nevexis.com>
2018-12-13 13:22:42 +0200

    replace syso with logger

--- src/test/java/com/github/evgdim/tdd/O1_BasicAnnotationTests.java
+++ src/test/java/com/github/evgdim/tdd/O1_BasicAnnotationTests.java
@@ -6,36 +6,38 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
 public class O1_BasicAnnotationTests {
 
     @BeforeClass
     public static void runOnceBeforeClass() {
-        System.out.println("@BeforeClass - runOnceBeforeClass");
+        log.info("@BeforeClass - runOnceBeforeClass");
     }
 
     @AfterClass
     public static void runOnceAfterClass() {
-        System.out.println("@AfterClass - runOnceAfterClass");
+    	log.info("@AfterClass - runOnceAfterClass");
     }
 
     @Before
     public void runBeforeTestMethod() {
-        System.out.println("    @Before - runBeforeTestMethod");
+    	log.info("    @Before - runBeforeTestMethod");
     }
 
     @After
     public void runAfterTestMethod() {
-        System.out.println("    @After - runAfterTestMethod");
+    	log.info("    @After - runAfterTestMethod");
     }
 
     @Test
     public void test_method_1() {
-        System.out.println("        @Test - test_method_1 :: " + this);
+    	log.info("        @Test - test_method_1 :: " + this);
     }
 
     @Test
     public void test_method_2() {
-        System.out.println("        @Test - test_method_2 :: " + this);
+    	log.info("        @Test - test_method_2 :: " + this);
     }
 
 }
