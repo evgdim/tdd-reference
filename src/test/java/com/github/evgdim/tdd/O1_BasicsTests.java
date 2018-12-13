b41c16d7586ba7f97ac5e11cb83b0609281af862
Evgeni DImitrov <evgeni.dimitrov@nevexis.com>
2018-12-13 13:35:42 +0200

    add Rule example

--- src/test/java/com/github/evgdim/tdd/O1_BasicsTests.java
+++ src/test/java/com/github/evgdim/tdd/O1_BasicsTests.java
@@ -4,12 +4,18 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
 
 import lombok.extern.slf4j.Slf4j;
 @Slf4j
-public class O1_BasicAnnotationTests {
-
+public class O1_BasicsTests {
+	@Rule
+	public MyCustomRule myRule = new MyCustomRule();
+	
     @BeforeClass
     public static void runOnceBeforeClass() {
         log.info("@BeforeClass - runOnceBeforeClass");
@@ -40,4 +46,22 @@ public class O1_BasicAnnotationTests {
     	log.info("        @Test - test_method_2 :: " + this);
     }
 
+}
+
+@Slf4j
+class MyCustomRule implements TestRule {
+
+	@Override
+	public Statement apply(Statement base, Description description) {
+		return new Statement() {
+			
+			@Override
+			public void evaluate() throws Throwable {
+				log.info("               @MyCustomRule before {}", description);
+				base.evaluate();
+				log.info("               @MyCustomRule after {}", description);
+			}
+		};
+	}
+	
 }
