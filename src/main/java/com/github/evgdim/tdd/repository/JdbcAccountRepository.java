bf9464abf846ca0f7d134d890231ad4dc231dafe
Evgeni DImitrov <evgeni.dimitrov@nevexis.com>
2018-12-13 16:49:59 +0200

    imporve readability

--- src/main/java/com/github/evgdim/tdd/repository/JdbcAccountRepository.java
+++ src/main/java/com/github/evgdim/tdd/repository/JdbcAccountRepository.java
@@ -17,12 +17,15 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class JdbcAccountRepository implements AccountRepository{
 	private final NamedParameterJdbcTemplate jdbc;
+	
 	@Override
 	public Optional<Account> findById(Long id) {
 		MapSqlParameterSource paramMap = new MapSqlParameterSource();
 		paramMap.addValue("id", id, Types.NUMERIC);
+		
 		List<Account> accounts = jdbc.query("select number, amount from account where id = :id", paramMap, 
 				(RowMapper<Account>) (rs, rowNum) -> new Account(id, rs.getString("number"), rs.getBigDecimal("amount")));
+		
 		return accounts != null && accounts.size() > 0 ? Optional.of(accounts.get(0)) : Optional.empty();
 	}
 
--- src/main/java/com/github/evgdim/tdd/service/AccountClientImpl.java
+++ src/main/java/com/github/evgdim/tdd/service/AccountClientImpl.java
@@ -23,9 +23,11 @@ public class AccountClientImpl implements AccountClient {
 
 	@Override
 	public List<Account> findAccounts(Long id) {
+		ParameterizedTypeReference<List<Account>> type = new ParameterizedTypeReference<List<Account>>(){};
 		ResponseEntity<List<Account>> entity = restTemplate.exchange("/accounts/{id}", 
-				HttpMethod.GET, null, new ParameterizedTypeReference<List<Account>>(){}, id);
-		if(entity.getStatusCode() != HttpStatus.OK) {
+				HttpMethod.GET, null, type, id);
+		
+		if(entity.getStatusCode() != HttpStatus.OK) { 
 			throw new AccountsServiceException(entity);
 		}
 		return entity.getBody();
