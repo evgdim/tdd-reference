f19fd1caf86f2e98ff78a48c302a863365ccdbda
Evgeni DImitrov <evgeni.dimitrov@nevexis.com>
2018-12-13 16:26:50 +0200

    cleanup

--- src/test/java/com/github/evgdim/tdd/O4_FullRunningServerTests.java
+++ src/test/java/com/github/evgdim/tdd/O4_FullRunningServerTests.java
@@ -13,14 +13,11 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.github.evgdim.tdd.entity.Person;
